6bf21492d889c86bc662e482361668ea749d48a5
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-21 18:32:52 +0300

    task 2

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""module to read a csv file"""
+import os
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """ Function to  to return in a list for pagination parameters.
+    param page: start of the page
+    param page_size: the size of a page
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get the cached pages
+        """
+        result = []
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        for x in range(start, end):
+            try:
+                result.append(self.__dataset[x])
+            except IndexError:
+                return []
+        return result
