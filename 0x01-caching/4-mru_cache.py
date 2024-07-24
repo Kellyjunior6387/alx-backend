c87bb52d9bdd07d0daa6b4eabf035d6e1524b5bf
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-24 10:40:32 +0300

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Module to implement the get and put methods"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """inherits for BaseCaching and implement put and get
+    """
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Assign to item to key in cached_data
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.add_key(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    last = self.order.pop()
+                    del self.cache_data[last]
+                    print(f"DISCARD: {last}")
+                self.cache_data[key] = item
+                self.order.append(key)
+
+    def get(self, key):
+        """Get the value matching the key
+        """
+        if key in self.cache_data.keys():
+            self.add_key(key)
+            return self.cache_data[key]
+        else:
+            return None
+
+    def add_key(self, key):
+        """ Method to track recently used key
+        """
+        if key in self.order:
+            self.order.remove(key)
+        self.order.append(key)
