b4530a751f166847afbd2a70b64ae250c7dcb7df
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-30 14:41:53 +0300

    task 3

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -17,13 +17,13 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> None:
     """Function to render 0-index template"""
     return render_template('3-index.html')
 
