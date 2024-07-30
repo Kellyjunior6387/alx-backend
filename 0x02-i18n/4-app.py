6c423f67ee4c80c32e8ceee8170616cacd5bd914
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-30 14:50:21 +0300

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,31 +2,34 @@
 """Module to config babel"""
 from flask import Flask, render_template, g, request
 from flask_babel import Babel
-app.url_map.strict_slashes = False
 app = Flask(__name__)
 
 
 class Config():
     """The default configurations"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """Determine the best match with our supported languages."""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
     """Function to render 0-index template"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
